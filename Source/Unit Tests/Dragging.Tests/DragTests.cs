15865a1c31292523ea780390829748478d7263d3
Justin Smith <justin.smith@glg.com>
2010-01-12 10:59:07 -0800

    [initial commit]

---
+++ Source/Unit Tests/Dragging.Tests/DragTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace JSmith.Dragging.Tests
+{
+    [TestFixture]
+    public class DragTests
+    {
+        /*[Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+         
+        }//end method
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue(true);
+
+        }//end method
+        */
+
+    }//end class
+
+}//end namespace
